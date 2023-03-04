d3ada5a4310ee00918ebf3e8af4573a613033a85
BlackKingI <Demchenko.Valera2015@yandex.ru>
2023-03-04 17:47:53 +0400

    Init Commit

--- task_0/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ task_0/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.text = """
+            APPLICATION_ID = ${BuildConfig.APPLICATION_ID}
+            Build type = ${BuildConfig.BUILD_TYPE}
+            FLAVOR = ${BuildConfig.FLAVOR}
+            VERSION_CODE = ${BuildConfig.VERSION_CODE}
+            VERSION_NAME = ${BuildConfig.VERSION_NAME} 
+        """.trimIndent()
     }
 }
