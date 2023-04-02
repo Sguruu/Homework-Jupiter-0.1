a92b4af533d7d1e4ca8fdf2c3f3af35e16c798d7
sergejmehonosin <sergey01051997@mail.ru>
2023-04-02 13:23:29 +0300

    Вынесение логики в функцию

--- task_5/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ task_5/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,83 +11,84 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var editTextLogin:EditText
-    private lateinit var editTextPassword:EditText
-    private lateinit var accept:CheckBox
-    private lateinit var buttonLogin:Button
-    private lateinit var wheel:ProgressBar
-    private var check1 = false
-    private var check2 = false
+    private lateinit var editTextLogin: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var acceptCheckBox: CheckBox
+    private lateinit var buttonLogin: Button
+    private lateinit var wheel: ProgressBar
+    private var checkEditTextLoginNoEmpty = false
+    private var checkEditTextPasswordNoEmpty = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
 
-        editTextLogin.addTextChangedListener(object : TextWatcher{
+        editTextLogin.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                check1 = p0?.isNotEmpty()?: false
-                buttonLogin.isEnabled = check1 && check2 && accept.isChecked
+                checkEditTextLoginNoEmpty = p0?.isNotEmpty() ?: false
+                renderButtonEnabled()
             }
+
             override fun afterTextChanged(p0: Editable?) {}
         })
 
-        editTextPassword.addTextChangedListener(object : TextWatcher{
+        editTextPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                check2 = p0?.isNotEmpty()?: false
-                buttonLogin.isEnabled = check1 && check2 && accept.isChecked
+                checkEditTextPasswordNoEmpty = p0?.isNotEmpty() ?: false
+                renderButtonEnabled()
             }
+
             override fun afterTextChanged(p0: Editable?) {
             }
         })
 
-        accept.setOnCheckedChangeListener{ _, isChecked ->
-            if (isChecked){
-                buttonLogin.isEnabled = check1 && check2
-            }else{
-                buttonLogin.isEnabled = false
-            }
+        acceptCheckBox.setOnCheckedChangeListener { _, _ ->
+            renderButtonEnabled()
         }
 
-
-        buttonLogin.setOnClickListener{
+        buttonLogin.setOnClickListener {
             startWheel()
         }
     }
 
-    private fun initView(){
+    private fun initView() {
         editTextLogin = findViewById(R.id.edit_login)
         editTextPassword = findViewById(R.id.edit_password)
-        accept = findViewById(R.id.accept)
+        acceptCheckBox = findViewById(R.id.accept)
         buttonLogin = findViewById(R.id.button_login)
         wheel = findViewById(R.id.wheel)
     }
 
-    private fun startWheel(){
-     wheel.visibility = View.VISIBLE
+    private fun startWheel() {
+        wheel.visibility = View.VISIBLE
         editTextLogin.isEnabled = false
         editTextPassword.isEnabled = false
-        accept.isEnabled = false
+        acceptCheckBox.isEnabled = false
         buttonLogin.isEnabled = false
 
         Handler(Looper.getMainLooper()).postDelayed({
             wheel.visibility = View.GONE
             editTextLogin.isEnabled = true
             editTextPassword.isEnabled = true
-            accept.isEnabled = true
+            acceptCheckBox.isEnabled = true
             buttonLogin.isEnabled = true
             Toast.makeText(
                 this,
                 resources.getString(R.string.successfully),
-                Toast.LENGTH_SHORT).show()
-        },2000)
+                Toast.LENGTH_SHORT
+            ).show()
+        }, 2000)
     }
 
+    private fun renderButtonEnabled() {
+        buttonLogin.isEnabled =
+            checkEditTextLoginNoEmpty && checkEditTextPasswordNoEmpty && acceptCheckBox.isChecked
+    }
 }
