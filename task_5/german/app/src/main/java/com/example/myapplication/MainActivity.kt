34c4c2779f8425222bd7f24dea85328f4776eaed
Huawei <tretiak.g.k@mail.ru>
2023-04-09 22:41:18 +1100

    Сохранение готового проекта

--- task_5/german/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ task_5/german/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,88 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.view.View
+import android.widget.*
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+    lateinit var inputEmail: EditText
+    lateinit var inputPassword: EditText
+    lateinit var checkBoxAgree: CheckBox
+    lateinit var buttonOk: Button
+    lateinit var progressBar: ProgressBar
+
+    private var checkEmailEmptyorNo = false
+    private var checkPasswordEmptyorNo = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        find()
+
+        inputEmail.addTextChangedListener(object : overTextWatch() {
+            override fun onTextChanged(write: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkEmailEmptyorNo = write?.isNotEmpty()?: false
+                emptyOrNo()
+            }
+        })
+
+        inputPassword.addTextChangedListener(object : overTextWatch() {
+            override fun onTextChanged(write: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkPasswordEmptyorNo = write?.isNotEmpty()?: false
+                emptyOrNo()
+            }
+        })
+
+        checkBoxAgree.setOnCheckedChangeListener { _, _ ->
+            emptyOrNo()
+        }
+
+        buttonOk.setOnClickListener {
+            starting()
+        }
+    }
+    fun find() {
+        textView = findViewById(R.id.TextView)
+        inputEmail = findViewById(R.id.EditTextMail)
+        inputPassword = findViewById(R.id.EditTextPassword)
+        checkBoxAgree = findViewById(R.id.CheckBox)
+        buttonOk = findViewById(R.id.Button)
+        progressBar = findViewById(R.id.ProgressBar)
+    }
+
+    abstract class overTextWatch : android.text.TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun afterTextChanged(p0: Editable?) {}
+    }
+    private fun emptyOrNo(){
+        buttonOk.isEnabled =
+        checkEmailEmptyorNo && checkPasswordEmptyorNo && checkBoxAgree.isChecked
+    }
+
+    private fun starting() {
+        progressBar.visibility = View.VISIBLE
+        inputEmail.isEnabled = false
+        inputPassword.isEnabled = false
+        checkBoxAgree.isEnabled = false
+        buttonOk.isEnabled = false
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            progressBar.visibility = View.GONE
+            inputEmail.isEnabled = true
+            inputPassword.isEnabled = true
+            checkBoxAgree.isEnabled = true
+            buttonOk.isEnabled = true
+            Toast.makeText(
+                this,
+                resources.getString(R.string.jjj),
+                Toast.LENGTH_LONG
+            ).show()
+        },2000)
     }
 }
