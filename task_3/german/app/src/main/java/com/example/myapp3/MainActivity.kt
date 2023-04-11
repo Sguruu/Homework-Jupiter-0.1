1dc0ee443a8bfaf55323c7f6d3ef0df343e90541
Huawei <tretiak.g.k@mail.ru>
2023-04-12 08:05:36 +1100

    Изменения настроек в main

--- task_3/german/app/src/main/java/com/example/myapp3/MainActivity.kt
+++ task_3/german/app/src/main/java/com/example/myapp3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text_view = findViewById<TextView>(R.id.textView)
+        text_view.text = """
+            APPLICATION_ID = ${BuildConfig.APPLICATION_ID}
+            Build type = ${BuildConfig.BUILD_TYPE}
+            FLAVOR = ${BuildConfig.FLAVOR}
+            VERSION_CODE = ${BuildConfig.VERSION_CODE}
+            VERSION_NAME = ${BuildConfig.VERSION_NAME} 
+        """.trimIndent()
     }
 }
