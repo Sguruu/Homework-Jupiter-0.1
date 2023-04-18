2a29edeba63f7553ca516b26c2cddbd49ea5be10
tretiakIT <127021928+tretiakIT@users.noreply.github.com>
2023-04-18 16:20:00 +1100

    Вывод конфигураций на экран

--- task_3/german/app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ task_3/german/app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text_view = findViewById<TextView>(R.id.textView)
+
+        text_view.text = """
+            Version name = ${BuildConfig.VERSION_NAME}
+            Flavor = ${BuildConfig.FLAVOR}
+            Build Type = ${BuildConfig.BUILD_TYPE}
+            APPLICATION_ID = ${BuildConfig.APPLICATION_ID}
+        """.trimIndent()
     }
 }
