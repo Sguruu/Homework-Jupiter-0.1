0fd53767f84f2450bd09755e72b1709c3ad26ef2
tretiakIT <127021928+tretiakIT@users.noreply.github.com>
2023-04-21 15:12:26 +1100

    MAIN ACTIVITY

--- task_3/german/app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ task_3/german/app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = """
+            APPLICATION_ID = ${BuildConfig.APPLICATION_ID}
+            VERSION NAME = ${BuildConfig.VERSION_NAME}
+            VERSION CODE = ${BuildConfig.VERSION_CODE}
+            FLAVOR = ${BuildConfig.FLAVOR}
+            BUILD TYPE = ${BuildConfig.BUILD_TYPE}
+        """.trimIndent()
     }
 }
