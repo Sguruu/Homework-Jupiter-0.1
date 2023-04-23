08445a6fe57854c52ec8854a5b00a7b540212a5f
tretiakIT <127021928+tretiakIT@users.noreply.github.com>
2023-04-23 13:00:26 +1100

    Завершение домашней работы 7.1

--- task_7_1/german/app/src/main/java/com/example/myapplication71/MainActivity.kt
+++ task_7_1/german/app/src/main/java/com/example/myapplication71/MainActivity.kt
@@ -1,11 +1,96 @@
 package com.example.myapplication71
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private lateinit var textView: TextView
+    private lateinit var inputEmail: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var checkBoxAgree: CheckBox
+    private lateinit var buttonOk: Button
+    private lateinit var progressBar: ProgressBar
+
+    private var checkEmailEmptyorNo = false
+    private var checkPasswordEmptyorNo = false
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val orientation = resources.configuration.orientation
+
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                setContentView(R.layout.activity_main)
+
+            } else {
+                setContentView(R.layout.activity_second)
+            }
+            find()
+
+            inputEmail.addTextChangedListener(object : overTextWatch() {
+                override fun onTextChanged(write: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    checkEmailEmptyorNo = write?.isNotEmpty()?: false
+                    emptyOrNo()
+                }
+            })
+
+            inputPassword.addTextChangedListener(object : overTextWatch() {
+                override fun onTextChanged(write: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    checkPasswordEmptyorNo = write?.isNotEmpty()?: false
+                    emptyOrNo()
+                }
+            })
+
+            checkBoxAgree.setOnCheckedChangeListener { _, _ ->
+                emptyOrNo()
+            }
+
+            buttonOk.setOnClickListener {
+                starting()
+            }
+        }
+        private fun find() {
+            textView = findViewById(R.id.textView)
+            inputEmail = findViewById(R.id.inputEmail)
+            inputPassword = findViewById(R.id.inputPassword)
+            checkBoxAgree = findViewById(R.id.checkAgreement)
+            buttonOk = findViewById(R.id.buttonOk)
+            progressBar = findViewById(R.id.loadingBar)
+        }
+
+        abstract class overTextWatch : android.text.TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+        }
+        private fun emptyOrNo(){
+            buttonOk.isEnabled = checkEmailEmptyorNo && checkPasswordEmptyorNo && checkBoxAgree.isChecked
+        }
+
+        private fun starting() {
+            progressBar.visibility = View.VISIBLE
+            inputEmail.isEnabled = false
+            inputPassword.isEnabled = false
+            checkBoxAgree.isEnabled = false
+            buttonOk.isEnabled = false
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                progressBar.visibility = View.GONE
+                inputEmail.isEnabled = true
+                inputPassword.isEnabled = true
+                checkBoxAgree.isEnabled = true
+                buttonOk.isEnabled = true
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.load),
+                    Toast.LENGTH_LONG
+                ).show()
+            },2000)
+        }
     }
-}
