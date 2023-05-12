bed2f06b205d474be9e1c0a64a22c8870bf76446
BlackKingI <124813003+BlackKingI@users.noreply.github.com>
2023-05-12 16:18:29 +0400

    Добавлена возможность добавлять и просматривать список друзей

--- task_7_3/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ task_7_3/valera/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,42 +7,47 @@ import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
+import android.widget.TextView
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ShowerListBinding
 
 class MainActivity : AppCompatActivity() {
     private var checkEditTextNameNoEmpty = false
     private var checkEditTextSurnameNoEmpty = false
     private var checkEditTextPhoneNumberNoEmpty = false
     private lateinit var binding: ActivityMainBinding
+    private val friendList = mutableListOf<Friend>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initToolbar()
     }
-    fun initToolbar(){
+    private fun initToolbar(){
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId){
 
                 R.id.searcher->{true}
 
                 R.id.adder ->{
-                    binding.welcomeWords.visibility = View.GONE
-                    binding.adderScroll.visibility = View.VISIBLE
-                    adder_friend()
-
+                    adderFriend()
                     true}
 
-                R.id.shower->{true}
-
-                R.id.joke_listener->{true}
+                R.id.shower->{
+                    showerList()
+                    true}
 
                 else ->{false}
             }
         }
     }
-    fun adder_friend(){
+    private fun adderFriend(){
+        binding.welcomeWords.visibility = View.GONE
+        binding.showerList.visibility = View.GONE
+        binding.adderFriend.visibility = View.VISIBLE
 
         binding.editName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -75,7 +80,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.adderButtom.setOnClickListener {
             startWheel()
-
+            addFriendOnList()
+            finishWheel()
         }
     }
 
@@ -85,7 +91,28 @@ class MainActivity : AppCompatActivity() {
         binding.edinSurname.isEnabled = false
         binding.editPhoneNumber.isEnabled = false
         binding.adderButtom.isEnabled = false
+    }
 
+    private fun addFriendOnList(){
+            friendList.add(Friend(
+                binding.editName.text.toString(),
+                binding.edinSurname.text.toString(),
+                binding.editPhoneNumber.text.toString()))
+
+        val view = ShowerListBinding.inflate(layoutInflater, binding.showerList, false)
+
+        view.apply {
+            view.friendName.text = binding.editName.text.toString()
+            view.friendSurname.text = binding.edinSurname.text.toString()
+            view.friendPhoneNumber.text = binding.editPhoneNumber.text.toString()
+            view.buttomDellFriend.setOnClickListener {
+                binding.showerList.removeView(view.root)
+            }
+        }
+        binding.showerList.addView(view.root)
+    }
+
+    private fun finishWheel(){
         Handler(Looper.getMainLooper()).postDelayed({
             binding.wheel.visibility = View.GONE
             binding.editName.isEnabled = true
@@ -105,5 +132,22 @@ class MainActivity : AppCompatActivity() {
     private fun renderButtonEnabled() {
         binding.adderButtom.isEnabled =
             checkEditTextNameNoEmpty && checkEditTextSurnameNoEmpty && checkEditTextPhoneNumberNoEmpty
+    }
+
+    private fun showerList(){
+//        binding.showerList.removeAllViews()
+        binding.welcomeWords.visibility = View.GONE
+        binding.adderFriend.visibility = View.GONE
+        binding.showerList.visibility = View.VISIBLE
+
+//        val view = layoutInflater.inflate(R.layout.shower_list, binding.showList, false)
+//
+//        view.apply {
+//            binding.friend.text = "бла бла бла"
+//            binding.buttomDellFriend.setOnClickListener {
+//                binding.showerList.removeView(view)
+//            }
+//        }
+//        binding.showerList.addView(view)
     }
 }
