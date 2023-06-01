9bafbfa66505bb89ca7ba35194bf69a7058009eb
sergejmehonosin <sergey01051997@mail.ru>
2023-06-01 22:17:55 +0300

    Task 12 - выполнение задачи

--- task_11/serg/app/src/main/java/com/weather/task7_3notebook/MainActivity.kt
+++ task_11/serg/app/src/main/java/com/weather/task7_3notebook/MainActivity.kt
@@ -1,63 +1,77 @@
 package com.weather.task7_3notebook
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.MenuItem
-import android.view.View
 import android.widget.SearchView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.appbar.AppBarLayout.LayoutParams.*
 import com.weather.task7_3notebook.databinding.ActivityMainBinding
-import com.weather.task7_3notebook.databinding.ItemContactBinding
 
-class MainActivity : AppCompatActivity() {
+interface IMainActivity {
+    /**
+     * Добавить контакт в список MainActivity
+     */
+    fun addContact(contact: Contact)
+
+    /**
+     * Добавить удаление контактка из списка MainActivity
+     */
+    fun removeContact(contact: Contact)
+
+    /**
+     * Получить список контактов из MainActivity
+     */
+    fun getListContact(): List<Contact>
+}
+
+class MainActivity : AppCompatActivity(), IMainActivity {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var toast: Toast
     private val listContact = mutableListOf<Contact>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // отключение темной темы
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        toast = Toast(this)
-
+        if (savedInstanceState == null) {
+            showAddPersonFragment()
+        }
+        // отключение темной темы
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         initListener()
     }
 
+    override fun addContact(contact: Contact) {
+        listContact.add(contact)
+    }
+
+    override fun removeContact(contact: Contact) {
+        listContact.remove(contact)
+    }
+
+    override fun getListContact(): List<Contact> {
+        return listContact
+    }
+
     private fun initListener() {
-        binding.buttonAddContact.setOnClickListener {
-            val name = binding.editTextName.text.toString()
-            val lastName = binding.editTextLastName.text.toString()
-            val numberPhone = binding.editTextNumber.text.toString().toIntOrNull() ?: 0
-
-            binding.editTextName.setText("")
-            binding.editTextLastName.setText("")
-            binding.editTextNumber.setText("")
-
-            listContact.add(Contact(name, lastName, numberPhone))
-            renderProgressBar(resources.getString(R.string.save_contact))
-        }
-
         binding.toolbar.setOnMenuItemClickListener { itemMenu ->
             when (itemMenu.itemId) {
                 R.id.actionAddContact -> {
-                    renderAddContact()
+                    replaceAddPersonFragment()
                     true
                 }
+
                 R.id.actionShowContent -> {
-                    renderShowContactAll()
+                    replaceListFragment()
                     true
                 }
+
                 R.id.actionSearch -> {
-                    renderActionSearch()
+                    replaceListFragment()
+                    // renderActionSearch()
                     true
                 }
+
                 else -> {
                     false
                 }
@@ -83,13 +97,18 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onQueryTextChange(p0: String?): Boolean {
-                    // удаление всех вью
-                    binding.linearLayoutContent.removeAllViews()
                     // простой поиск по имени по нажатию кнопки
                     listContact.filter { it.name.contains(p0 ?: "", ignoreCase = true) }
                         .let { filterList ->
-                            filterList.forEach { contact ->
-                                createViewContent(contact)
+                            // проверка что сейчас показывается нужный фрагмент
+                            val currentFragment =
+                                supportFragmentManager.findFragmentById(
+                                    R.id.fragmentContainerView1
+                                )
+                            if (currentFragment != null &&
+                                currentFragment.tag == ListFragment.FRAGMENT_TAG
+                            ) {
+                                (currentFragment as IListFragment).processSearchResult(filterList)
                             }
                         }
                     return true
@@ -97,69 +116,39 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
-    private fun renderAddContact() {
-        binding.linearLayoutCreateUser.visibility = View.VISIBLE
-        binding.linearLayoutContent.visibility = View.GONE
+    private fun replaceListFragment() {
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(
+                binding.fragmentContainerView1.id,
+                ListFragment(),
+                ListFragment.FRAGMENT_TAG
+            )
+            .commit()
     }
 
-    private fun renderShowContactAll() {
-        // показать весь список
-        binding.linearLayoutContent.removeAllViews()
-        binding.linearLayoutCreateUser.visibility = View.GONE
-        binding.linearLayoutContent.visibility = View.VISIBLE
+    private fun showAddPersonFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(
+                binding.fragmentContainerView1.id,
+                AddPersonFragment(),
+                AddPersonFragment.FRAGMENT_TAG
+            )
+            .setReorderingAllowed(true)
+            .commit()
+    }
 
-        listContact.forEach { contact ->
-            createViewContent(contact)
+    private fun replaceAddPersonFragment() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView1)
+        if (currentFragment != null && currentFragment.tag != AddPersonFragment.FRAGMENT_TAG) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(
+                    binding.fragmentContainerView1.id,
+                    AddPersonFragment(),
+                    AddPersonFragment.FRAGMENT_TAG
+                )
+                .commit()
         }
-    }
-
-    private fun renderActionSearch() {
-        binding.linearLayoutCreateUser.visibility = View.GONE
-        binding.linearLayoutContent.visibility = View.VISIBLE
-    }
-
-    private fun createViewContent(contact: Contact) {
-        // val view = layoutInflater.inflate(R.layout.item_contact, binding.linearLayoutContent, false)
-        val view = ItemContactBinding.inflate(layoutInflater, binding.linearLayoutRoot, false)
-        view.textViewName.text = contact.name
-        view.textViewLastName.text = contact.lastName
-        view.textViewNumberPhone.text = contact.number.toString()
-
-        view.buttonDelete.setOnClickListener {
-            listContact.remove(contact)
-            binding.linearLayoutContent.removeView(view.root)
-        }
-
-        binding.linearLayoutContent.addView(view.root)
-    }
-
-    private fun renderProgressBar(text: String) {
-        binding.progressBar.visibility = View.VISIBLE
-        binding.buttonAddContact.isEnabled = false
-        Handler(Looper.getMainLooper()).postDelayed({
-            binding.progressBar.visibility = View.GONE
-            binding.buttonAddContact.isEnabled = true
-            toast(text)
-        }, 2000)
-    }
-
-    // пример изменение флага через Toolbar
-//    private fun renderScrollFlagsToolbar(isActiveScroll: Boolean) {
-//        val params = (binding.toolbar.layoutParams as AppBarLayout.LayoutParams)
-//
-//        when (isActiveScroll) {
-//            true -> {
-//                params.scrollFlags = SCROLL_FLAG_SCROLL + SCROLL_FLAG_ENTER_ALWAYS
-//            }
-//            false -> {
-//                params.scrollFlags = SCROLL_FLAG_NO_SCROLL
-//            }
-//        }
-//    }
-
-    private fun toast(text: String) {
-        toast.cancel()
-        toast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
-        toast.show()
     }
 }
