f61306cbf4d0084592da8b24f43c3c5d8dac00cc
BlackKingI <124813003+BlackKingI@users.noreply.github.com>
2023-06-17 12:53:01 +0400

    Оформление второго экрана задача 10_01_02

--- task_10_01/valera/app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ task_10_01/valera/app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -1,12 +1,30 @@
 package com.example.myapplication
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonOpenCallManager.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_DIAL).apply {
+                val enterPhoneNumber = binding.editPhoneNumber.text.toString()
+                data = Uri.parse("tel:$enterPhoneNumber")
+            }
+            try {
+                startActivity(callIntent)
+            } catch (e: ActivityNotFoundException){
+                Toast.makeText(this, "App for call not found", Toast.LENGTH_SHORT).show()
+            }
 
+        }
     }
 }
