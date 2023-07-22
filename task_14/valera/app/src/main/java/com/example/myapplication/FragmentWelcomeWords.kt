ce77e2b5524d28870a68258512cd6e94bed50447
BlackKingI <124813003+BlackKingI@users.noreply.github.com>
2023-07-22 11:39:47 +0400

    Начало выполнения задачи 14_02

---
+++ task_14/valera/app/src/main/java/com/example/myapplication/FragmentWelcomeWords.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentWelkomWordsBinding
+
+class FragmentWelcomeWords : Fragment() {
+    private var _binding : FragmentWelkomWordsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelkomWordsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
