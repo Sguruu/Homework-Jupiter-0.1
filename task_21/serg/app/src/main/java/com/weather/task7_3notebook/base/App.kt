19c097da0a18b9bcf216a5225f4797db4eeaaeae
sergejmehonosin <sergey01051997@mail.ru>
2023-08-02 23:12:13 +0300

    [Homework_21] - Инициализация DB

--- task_21/serg/app/src/main/java/com/weather/task7_3notebook/base/App.kt
+++ task_21/serg/app/src/main/java/com/weather/task7_3notebook/base/App.kt
@@ -2,11 +2,13 @@ package com.weather.task7_3notebook.base
 
 import android.app.Application
 import android.content.res.Resources
+import com.weather.task7_3notebook.base.db.DataBase
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         res = this.resources
+        DataBase.init(this)
     }
 
     companion object {
